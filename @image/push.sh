c80ccdee7baaec5bf1f62accb369891ddc562ec4
momashanhe <momashanhe@163.com>
2024-05-19 09:20:33 +0800

    Site updated: 2024-05-19 09:20:30

--- @image/push.sh
+++ @image/push.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-cd /d/Work/WebSite/MoMaShanHe/source/@image
-rm -rf .git
-git init
-git config --local user.name momashanhe
-git config --local user.email momashanhe@163.com
-git remote add origin git@momashanhe:momashanhe/momashanhe-image.git
-git branch -M main
+# cd /d/Work/WebSite/momashanhe/source/@image
+# rm -rf .git
+# git init
+# git config --local user.name momashanhe
+# git config --local user.email momashanhe@163.com
+# git remote add origin git@momashanhe:momashanhe/momashanhe-image.git
+# git branch -M main
+# git add .
+# git commit -m "init commit on $(date +'%Y-%m-%d %H:%M:%S')"
+# git push -f origin main
+
+cd /d/Work/WebSite/momashanhe/source/@image
 git add .
-git commit -m "init commit on $(date +'%Y-%m-%d %H:%M:%S')"
-git push -f origin main
+git commit -m "update commit on $(date +'%Y-%m-%d %H:%M:%S')"
+git push origin main
--- @static/push.sh
+++ @static/push.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-cd /d/Work/WebSite/MoMaShanHe/source/@static
-rm -rf .git
-git init
-git config --local user.name momashanhe
-git config --local user.email momashanhe@163.com
-git remote add origin git@momashanhe:momashanhe/momashanhe-static.git
-git branch -M main
-git add .
-git commit -m "init commit on $(date +'%Y-%m-%d %H:%M:%S')"
-git push -f origin main
+# cd /d/Work/WebSite/momashanhe/source/@static
+# rm -rf .git
+# git init
+# git config --local user.name momashanhe
+# git config --local user.email momashanhe@163.com
+# git remote add origin git@momashanhe:momashanhe/momashanhe-static.git
+# git branch -M main
+# git add .
+# git commit -m "init commit on $(date +'%Y-%m-%d %H:%M:%S')"
+# git push -f origin main
 
+cd /d/Work/WebSite/momashanhe/source/@static
+git add .
+git commit -m "update commit on $(date +'%Y-%m-%d %H:%M:%S')"
+git push origin main
